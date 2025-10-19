5b3748a2df2a8d4bed1348421822f2f4242897ff
CYS0423 <3515415214@qq.com>
2025-10-19 12:28:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-input_str = input("请输入一行字符：")
-alpha_count = digit_count = space_count = other_count = 0
-for char in input_str:
-    if char.isalpha():
-        alpha_count += 1
-    elif char.isdigit():
-        digit_count += 1
-    elif char == ' ':
-        space_count += 1
-    else:
-        other_count += 1
-print("英文字符:", alpha_count)
-print("数字:", digit_count)
-print("空格:", space_count)
-print("其他字符:", other_count)
+def count_characters(input_string):
+    letter_count = 0
+    digit_count = 0
+    space_count = 0
+    other_count = 0
+
+    for char in input_string:
+        if char.isalpha():
+            letter_count += 1
+        elif char.isdigit():
+            digit_count += 1
+        elif char.isspace():
+            space_count += 1
+        else:
+            other_count += 1
+
+    return letter_count, digit_count, space_count, other_count
+
+def main():
+    input_string = input("请输入一行字符: ")
+    letters, digits, spaces, others = count_characters(input_string)
+    print(f"英文字符:{letters} 数字:{digits} 空格:{spaces} 其他字符:{others}")
+
+if __name__ == "__main__":
+    main()
